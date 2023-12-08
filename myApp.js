8c3f532c134730dfd8c69e5ab41f45b348354929
Murat Yilmaz <135061655+muratyilmaz729@users.noreply.github.com>
2023-12-08 16:45:54 +0300

    Add files via upload

---
+++ myApp.js
@@ -0,0 +1,73 @@
+let express = require('express');
+let app = express();
+
+let bodyParser = require('body-parser');
+
+/*
+console.log("Hello World");
+*/
+
+/*
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+*/
+
+//#4
+app.use("/public", express.static(__dirname + "/public"));
+
+//#11
+app.use(bodyParser.urlencoded({extended: false}))
+
+//#7
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next()
+})
+
+
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json"
+  });
+});
+
+
+//use the .env file
+app.get("/json", (req, res) => {
+
+  if (process.env["MESSAGE_STYLE"] == "uppercase") {
+    res.json({ "message":"HELLO JSON" })
+  } else {res.json({"message":"Hello json"}) 
+}
+})
+
+
+//#8
+app.get("/now", (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.json({"time":req.time})
+})
+
+//#9
+app.get("/:word/echo", (req, res) => {
+  res.json( {echo: req.params.word})
+})
+
+//#10
+app.get("/name", (req, res) => {
+  res.json({ name:req.query.first + " " + req.query.last})
+})
+  
+//#12
+app.post("/name", (req, res) => {
+  res.json({ name:req.body.first + " " + req.body.last})
+})
+
+
+
+
+
+ module.exports = app;
